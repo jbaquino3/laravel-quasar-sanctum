cc760a2204adab0bee2b8b9a04dd281ae4c140c6
DOT28 <jbaquino3@up.edu.ph>
2022-08-03 10:31:48 +0800

    sanctum csrf-token

--- quasar/src/App.vue
+++ quasar/src/App.vue
@@ -3,9 +3,24 @@
 </template>
 
 <script>
-	import { defineComponent } from 'vue'
+	import { defineComponent, ref } from 'vue'
+	import { api } from 'boot/axios'
+	import { useQuasar } from 'quasar'
 
 	export default defineComponent({
-		name: 'App'
+		name: 'App',
+
+		setup() {
+			const $q = useQuasar()
+
+			api.get("/sanctum/csrf-cookie").catch(() => {
+				$q.notify({
+					color: 'negative',
+					position: 'top',
+					message: 'Loading failed',
+					icon: 'report_problem'
+				})
+			})
+		}
 	})
 </script>
